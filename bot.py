f8a1d19d02d12371d5a67361f877e7e1c6d55493
Skyrim-police <65493638+Skyrim-police@users.noreply.github.com>
2020-09-13 19:54:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,17 +7,12 @@ from datetime import timezone, tzinfo, timedelta
 import time
 import time as timeModul
 import wikipedia
-from bs4 import BeautifulSoup
 import requests 
 import json
 import os 
 
 bot = commands.Bot(command_prefix='Скайрим ')
 bot.remove_command('help')
- 
-@bot.command()
-async def скажи(ctx, arg: str):
-  await ctx.send(arg)
  
 @bot.command()
 @commands.has_permissions(ban_members=True)
@@ -43,27 +38,6 @@ async def тест(ctx):
 @bot.command()
 async def инвайт(ctx): 
  await ctx.send( "https://discord.com/api/oauth2/authorize?client_id=707317001047244800&permissions=8&scope=bot")
- 
-@bot.command()
-async def юзер(ctx, Member: discord.Member = None ):
-    if not Member:
-        Member = ctx.author
-    roles = (role for role in Member.roles )
-    emb = discord.Embed(title='Инфа о участнике'.format(Member.name), description=f"Присоединился: {Member.joined_at.strftime('%b %#d, %Y')}\n\n "
-    f"Ник: {Member.name}\n\n"
- 
-   f"Кастомный ник: {Member.nick}\n\n"
-                                                                                      f"Статус: {Member.status}\n\n"
-                                                                                      f"ID: {Member.id}\n\n"
-                                                                                      f"Наивысшая роль: {Member.top_role}\n\n"
-                                                                                      f"Аккаунт создан: {Member.created_at.strftime('%b %#d, %Y')}", 
-                                                                                      color=0xc582ff, timestamp=ctx.message.created_at)
- 
-    emb.set_thumbnail(url= Member.avatar_url)
-    emb.set_footer(icon_url= Member.avatar_url)
-    emb.set_footer(text='Requested by: {}'.format(ctx.author.name), icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=emb)
- 
  
 @bot.command()
 async def монетка(ctx):
@@ -160,39 +134,6 @@ async def эмозя(ctx, emotie: discord.Emoji):
   emb = discord.Embed(title = '**Эмозя**', colour = color)
   emb.set_image(url = emotie.url)
   await ctx.send(embed = emb)
- 
- 
-class Currensy_grn:
-    DOLAR_UA = 'https://www.google.com/search?sxsrf=ALeKk03fHfLO2ZLgvH2aCYHyylK169bp7Q%3A1584807405441&ei=7T12Xt69Gsyf6ASQjpvADw&q=%D0%B4%D0%BE%D0%BB%D0%B0%D1%80%D0%B0+%D0%BA+%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D0%B5&oq=%D0%B4%D0%BE%D0%BB%D0%B0%D1%80%D0%B0+%D0%BA+%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D0%B5&gs_l=psy-ab.3..0i7i30j0i7i10i30l9.96278.96485..97742...0.2..0.99.195.2......0....1..gws-wiz.......0i71.ruuxORUhmxA&ved=0ahUKEwie6MDT-6voAhXMD5oKHRDHBvgQ4dUDCAs&uact=5'
- 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
- 
-    def get_currency_price(self):
-        full_page = requests.get(self.DOLAR_UA, headers=self.headers)
- 
-        soup = BeautifulSoup(full_page.content, 'html.parser')
- 
-        convert = soup.findAll('span', {'class': 'DFlfde', 
-                                        'class': 'SwHCTb',
-                                        'data-precision': 2})
-        global odna 
-        odna = convert[0].text
- 
-@bot.command()
-async def гривна(ctx):
-    color = 0xc582ff
-    cgrn =Currensy_grn()
-    cgrn.get_currency_price()
-    emb = discord.Embed(title = 'Курс гривны к доллару', colour = color)
-    emb.add_field(name = 'Курс', value = f'1$ = {odna}грн')
-    await ctx.send(embed = emb)
- 
- 
-@bot.command(aliases=['коронавирус'])
-async def корона(ctx, country):
-    for item in  json.loads(requests.get("http://covid2019-api.herokuapp.com/v2/current").text)['data']:
-        if item['location'] == country: 
-            return await ctx.send(embed=discord.Embed(title='Статистика Covid-19', description=f'**Данные по {country}:**\nУмерло: {item["deaths"]}\nВыздоровело: {item["recovered"]}\nЗаражено: {item["confirmed"]}',color=0xc582ff))
  
 @bot.command()
 @commands.has_permissions( administrator = True )
